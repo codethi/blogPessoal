fee1c1fc70dbf0841c4af084cabc557d9a699928
limathiagos <docencia.thiago@gmail.com>
2020-10-08 16:12:23 -0300

    Implementação de mais uma condição para evitar que o tema sejá atualizado se o campo descrição estiver vazio, arquivo put-tema.component.ts linha 40 e 41 adicionada

--- blogPessoal/src/app/put-tema/put-tema.component.ts
+++ blogPessoal/src/app/put-tema/put-tema.component.ts
@@ -37,6 +37,8 @@ export class PutTemaComponent implements OnInit {
     if (this.tema.postagem.length != 0) {
       this.alerta.showAlertDanger('Esse tema não pode ser modificado, pois já pertence a uma postagem.')
       this.router.navigate(['/cadastro-tema'])
+    } else if (this.tema.descricao == null || this.tema.descricao == ''){
+      this.alerta.showAlertDanger('A descrição não pode ficar vazia!')
     } else {
       this.temaService.putTema(this.tema).subscribe((resp: Tema) => {
         this.tema = resp
